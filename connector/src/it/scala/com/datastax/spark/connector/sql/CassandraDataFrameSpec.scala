5fbcd155726d428221e1813181780d49146c3ac9
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2026-02-24 15:30:09 -0400

    Remove notScylla skips for clustering order tests
    Scylla now correctly reports DESC clustering order in
    system_schema.columns. Verified on Scylla 2025.2.5 using
    the Python driver that clustering_order=DESC is properly
    returned for columns defined with DESC ordering.
    
    Ref scylladb/spark-scylladb-connector#25

--- connector/src/it/scala/com/datastax/spark/connector/sql/CassandraDataFrameSpec.scala
+++ connector/src/it/scala/com/datastax/spark/connector/sql/CassandraDataFrameSpec.scala
@@ -203,7 +203,7 @@ class CassandraDataFrameSpec extends SparkCassandraITFlatSpecBase with DefaultCl
 
   }
 
-  it should " be able to create a customized C* schema from a table" in notScylla("scylladb/spark-scylladb-connector#25: Clustering order on Scylla always reported as ASC") {
+  it should " be able to create a customized C* schema from a table" in {
     val df = spark
       .read
       .format("org.apache.spark.sql.cassandra")
